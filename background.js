0707b9446c2ff69cbaf0e5c18a25e030c3b50bff
ppuya13 <38575594+ppuya13@users.noreply.github.com>
2025-02-03 16:56:37 +0900

    ### 1.42 - 확장 프로그램 아이콘 클릭 시 크롬 웹 스토어 페이지로 이동하도록 수정

--- background.js
+++ background.js
@@ -1,3 +1,6 @@
 chrome.runtime.onInstalled.addListener(() => {
     console.log('DCInside URL Converter 확장 프로그램이 설치되었습니다.');
 });
+chrome.action.onClicked.addListener(() => {
+    chrome.tabs.create({ url: "https://chromewebstore.google.com/detail/dcinside-url-converter/bbhfiaigheddloefjfhfjkoijhjjefbc" });
+});
